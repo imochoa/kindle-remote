ba610f43bcdd8ee467c8d7e0dcc9103303442a5e
imochoa <nachomartinochoa@gmail.com>
2022-05-21 12:14:23 +0200

    fixed the POST requests

--- KindleRemote/KindleRemote.ino
+++ KindleRemote/KindleRemote.ino
@@ -1,12 +1,13 @@
-#include <ESP8266WiFi.h>
+//#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
-#include <WiFiClient.h>
 
-#ifndef STASSID
-#define STASSID ""
-#define STAPSK  ""
-#define HOSTNAME "esp8266"
-#endif
+
+const char* STASSID = "";
+const char* STAPSK = "";
+const char* HOSTNAME = "esp";
+
+ESP8266WebServer server(80);
 
 void setup(void) {
   Serial.begin(115200);
@@ -22,11 +23,10 @@ void setup(void) {
     Serial.print(".");
   }
   Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(STASSID);
+  Serial.println("Connected to "+String(STASSID));
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
+  
   // Set up mDNS responder:
   // - first argument is the domain name, in this example
   //   the fully-qualified domain name is "esp8266.local"
@@ -35,19 +35,38 @@ void setup(void) {
   if (!MDNS.begin(HOSTNAME)) {
     Serial.println("Error setting up MDNS responder!");
     while (1) {
-      delay(1000);
+      delay(500);
     }
   }
-  Serial.print("mDNS responder started at http://");
-  Serial.print(HOSTNAME);
-  Serial.println(".local");
+  Serial.print("mDNS responder started at http://"+String(HOSTNAME)+".local");
+
+  
+  server.onNotFound([](){
+    server.send(404, "text/plain", "Link wurde nicht gefunden!");  
+  });
+
+  server.on("/", []() {
+    server.send(200, "text/plain", "ESP-Startseite!");
+  });
+
+  server.on("/test", HTTP_POST, []() {
+    Serial.println("Triggered printPOST!");
+    String postBody = server.arg("plain");
+    Serial.println(postBody);
+
+    // Add a header to respond with a new location for the browser to go to the home page again
+    server.sendHeader("Location","/"); 
+    // Send it back to the browser with an HTTP status 303 (See Other) to redirect
+    server.send(303);
+  }); 
+
+  server.begin();
 
   // Add service to MDNS-SD
   MDNS.addService("http", "tcp", 80);
 }
 
 void loop(void) {
-
-  MDNS.update(); // Should be called in every loop to process MDNS
-
+  server.handleClient();  
+  MDNS.update();
 }
